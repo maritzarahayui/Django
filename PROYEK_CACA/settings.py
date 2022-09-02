9e213c0f81f96623af67e1f22af729328fde2353
maritzarahayu <maritzarahayui@gmail.com>
2022-09-02 20:50:32 +0700

    lieur

--- PROYEK_CACA/settings.py
+++ PROYEK_CACA/settings.py
@@ -47,6 +47,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'PROYEK_CACA.urls'
@@ -121,3 +122,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+
+ALLOWED_HOSTS = ["*"]
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn PROYEK_CACA.wsgi --log-file -
